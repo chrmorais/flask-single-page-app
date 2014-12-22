a7c6acfb3be7ce90f397d321ab97b893c56a8bbe
teachMeCode <brian.purcell7@gmail.com>
2014-12-22 15:43:42 -0800

    more fun with jQuery

--- app/app.py
+++ app/app.py
@@ -30,8 +30,11 @@ def home():
         # api call
         url = 'https://api.github.com/search/users?q=location:{0}+language:{1}'.format(value_one, value_two)
         # this changes the json reponse to a python dictionary
-        response_dict = requests.get(url).json()
-        return jsonify(response_dict)
+        try:
+            response_dict = requests.get(url).json()
+            return jsonify(response_dict)
+        except:
+            return jsonify({"error", "error message"}), 500
         # passing in an argument called 'String'
         # this is part of jinja templating
         # return render_template('index.html', value=total)
--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   console.log(' ready');
+  $('#try-again').hide();
 
   $("form").on("submit", function(){
     console.log("The form has been submitted");
@@ -13,14 +14,26 @@ $(document).ready(function(){
       url: "/",
       data: {first: valueOne, second: valueTwo},
       success: function(results){
-        // targeting the results id
-        console.log(results.items[0]);
-        $("#results").html('<a href="'+results.items[0].html_url+'">'+results.items[0].login+'</a><br><img src="'+results.items[0].avatar_url+'"+class="avatar">')
-        $("input").val("")
+          if (results.items.length > 0) {
+            $('input').hide();
+            $('#try-again').show();
+            var randNum = Math.floor(Math.random() * Object.keys(results.items).length)
+            // targeting the results id
+            console.log(results.items[randNum]);
+            $("#results").html('<a href="'+results.items[randNum].html_url+'" target="blank_"">'+results.items[randNum].login+'</a><br><img src="'+results.items[randNum].avatar_url+'" class="avatar">')
+            // $("input").val("")
+          } else {
+            $("#results").html("Something went wrong! Please try again")
+          }
       },
       error: function(error){
         console.log(error);
       }
     });
+  });
+  $('#try-again').on('click', function(){
+    $('input').val('').show();
+    $('#try-again').hide();
+    $('#results').html('');
   });
 }); //this is the end
